cdb62328de4514f7913b1eaad89e32f6712900ef
Jordan Sved <jordansved@gmail.com>
2014-03-20 18:17:35 -0400

    Initial commit

---
+++ adventure/src/main/java/com/djr/adventure/IndexActivity.java
@@ -0,0 +1,13 @@
+package com.djr.adventure;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IndexActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
