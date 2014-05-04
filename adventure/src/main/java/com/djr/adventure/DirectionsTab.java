b3d2601a8c870be1d61278f5b4631a10887bda42
danielbenaderet <danbenaderet@gmail.com>
2014-05-04 16:00:17 -0400

    updated directiontab to list results

--- adventure/src/main/java/com/djr/adventure/DirectionsTab.java
+++ adventure/src/main/java/com/djr/adventure/DirectionsTab.java
@@ -1,137 +1,49 @@
 package com.djr.adventure;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.Random;
 
 
-public class DirectionsTab extends Fragment {
+public class DirectionsTab extends ListFragment {
 
-    TextView mSearchResults;
+   // TextView mSearchResults;
 
+    private ArrayList<Business> businesses;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        Intent intent = getActivity().getIntent();
+        businesses = (ArrayList<Business>) intent.getExtras().get("EXTRA_DIRECTIONS_STEPS");
+        DirectionStepAdapter adapter = new DirectionStepAdapter(businesses);
+        setListAdapter(adapter);
+    }
 
-
-	}
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.directions_fragment, parent, false);
-        mSearchResults = (TextView) v.findViewById(R.id.searchResults);
-        Intent i = getActivity().getIntent();
-        ArrayList<Business> businesses = (ArrayList<Business>)i.getExtras().get("EXTRA_DIRECTIONS_STEPS");
-
-        String ret = "";
-        for (Business b : businesses) {
-            ret+= b.toString() + "\n";
+    private class DirectionStepAdapter extends ArrayAdapter<Business> {
+        public DirectionStepAdapter(ArrayList<Business> businesses) {
+            super(getActivity(), 0, businesses);
         }
-        mSearchResults.setText(ret);
-
-
-
-/*
-            HashMap<String, Boolean> params = (HashMap<String, Boolean>) i.getExtras().get("EXTRA_PREFERENCES_MAP");
-            String res = "";
-            for (String k : params.keySet()) {
-                if(params.get(k)) {
-                  try {
-
-
-                      YelpAsyncTask yat = new YelpAsyncTask();
-                      yat.execute(k);
-
-                      Business b = (Business) yat.get();
-                      res += b.toString() + "\n";
-
-                  }
-                  catch (Exception e) {
-                      Log.d("YelpSearch", e.toString());
-
-                  }
-                }
-            }
-
-        mSearchResults.setText(res);
-*/
-		return v;
-	}
-
-    private class YelpAsyncTask extends AsyncTask<String, Void, Object> {
-        private Exception exception;
 
         @Override
-        protected Object doInBackground(String... terms) {
-            Yelp y = Yelp.getYelp(getActivity());
-            String s = y.search(terms[0], 40.769280, -74.005185);
-            Log.w("YelpActivity", "Returned string is " + s);
-            try {
-                return getRandBusiness(s);
-//                    return processJson(s);
-            } catch (JSONException e) {
-                Log.d("YelpAsyncTask", "reached json exception" + e.getStackTrace().toString());
-                return e.toString();
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_step, null);
             }
-        }
 
-        protected void onPostExecute(Business result) {
-            Log.w("YelpActivity", "onPostExecute called. String param 'result' is " + result.toString());
-
-
+            // configure the view for this crime
+            Business b = getItem(position);
+            TextView directionText = (TextView)convertView.findViewById(R.id.direction_text);
+            directionText.setText(b.getName());
+            TextView directionTextInfo = (TextView)convertView.findViewById(R.id.direction_info_text);
+            directionTextInfo.setText(b.getLatitude() + "," + b.getLatitude());
+            return convertView;
         }
     }
-
-
-    Business getRandBusiness (String jsonStuff) throws JSONException {
-
-        int length = jsonStuff.length();
-        Random rand = new Random();
-        int randomBusiness = rand.nextInt(length < 5 ? length : 5);
-
-        JSONObject json = new JSONObject(jsonStuff);
-        JSONArray businesses = json.getJSONArray("businesses");
-        JSONObject center = json.getJSONObject("region").getJSONObject("center");
-        double lat = center.getDouble("latitude");
-        double lon = center.getDouble("longitude");
-
-        JSONObject business = businesses.getJSONObject(randomBusiness);
-
-        String name = business.getString("name");
-
-        Business ret = new Business(name, lon, lat);
-
-        return ret;
-
-
-    }
-
-    String processJson(String jsonStuff) throws JSONException {
-        JSONObject json = new JSONObject(jsonStuff);
-        JSONArray businesses = json.getJSONArray("businesses");
-        ArrayList<String> businessNames = new ArrayList<String>(businesses.length());
-        for (int i = 0; i < businesses.length(); i++) {
-            JSONObject business = businesses.getJSONObject(i);
-            businessNames.add(business.getString("name"));
-        }
-        return TextUtils.join("\n", businessNames);
-    }
-		
-	
 }
